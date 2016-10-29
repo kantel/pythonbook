6e9f7b709d8cfca7de02fc9235408f591e016707
Jörg Kantel <joerg@kantel.de>
2016-10-29 20:29:57 +0200

    Turtle-Test verbessert

--- content/sources/turtletest.py
+++ content/sources/turtletest.py
@@ -1,14 +1,15 @@
 import turtle
 
 wn = turtle.Screen()
-wn.bgcolor("black")        # set the window background color
+wn.bgcolor("black")
+wn.title("Hallo, Türtel!")
 
 tess = turtle.Turtle()
-tess.color("darkgreen")              # make tess blue
-tess.pensize(3)                 # set the width of her pen
+tess.color("darkgreen")
+tess.pensize(3)
 
 tess.forward(50)
 tess.left(120)
 tess.forward(50)
 
-wn.exitonclick()                # wait for a user click on the canvas
+wn.mainloop()
